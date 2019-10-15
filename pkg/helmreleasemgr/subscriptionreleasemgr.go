77845279bc7e39028967a150d06397eb7a95744c
Dominique Vernier <dominique.vernier@us.ibm.com>
2019-10-14 21:22:59 -0400

    Initial load on github.com (#3)
    * first commit
    
    * Create NewHelmManager
    
    * Implement SubscriptionRelease
    
    * Implement download and values
    
    * Fix releaseName
    
    * Use the ReleaseName as target for unzip
    
    * Use config for values instead of merge in dir.
    
    * Implement config and auth for helm-repo client
    
    * Size releasName not > than 52
    
    * Implement helm-repo monitoring
    
    * No need for finalizer, just check IsNotFound
    
    * regex for chart-name and keywords filtering
    
    * Implement status, secret helm-repo auth
    
    * Change Phase to status
    
    * Remove IBMMulticloudPlatform/subscription dep
    
    * Remove placementrule
    
    * Wrong test
    
    * Setup make
    
    * Fix lint
    
    * Add makefile.docker
    
    * 2 columns after image to overide
    
    * Add generate
    
    * add generate before build
    
    * install operator-sdk before generate
    
    * GO111MODULE=on
    
    * docker:login
    
    * fix git.apache.org
    
    * Fiix openapi name violation
    
    * Disable lint
    
    * Fix search on keywords
    
    * Fix tests
    
    * reactivate lint
    
    * remove image call
    
    * remove makefile.docker
    
    * Generate image with $(IMAGE_VERSION)
    
    * Fix configFile
    
    * type in Image name
    
    * Update description and summary
    
    * Try other arch
    
    * Install operator-sdk multi-arch
    
    * MOdule off to go get
    
    * Fix docker option
    
    * Active other arch only at pr merge
    
    * Limit to 32 chars
    
    * Set ossc target
    
    * struct for github
    
    * Fix chart path
    
    * Fix lint
    
    * Fix test
    
    * Change dest repo
    
    * Rename subscription to helmchartsubscription
    
    * rename subscriptionrelease to helmrelease
    
    * Update README
    
    * Add chartsSource in subscription to prepare github
    
    * enable mirroring for github
    
    * Prepare to move to github.com
    
    * Fix lint
    
    * Force image creation for test wiithout merge
    
    * Try again
    
    * add go_import_path
    
    * Remove build image test
    
    * Remove travis.yaml
    
    * Add readme
    
    * Add myself as owner
    
    * Add travis yaml
    
    * Remove hardening
    
    * Remove make init

---
+++ pkg/helmreleasemgr/subscriptionreleasemgr.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helmreleasemgr
+
+import (
+	"io/ioutil"
+
+	"os"
+
+	appv1alpha1 "github.com/IBM/multicloud-operators-subscription-release/pkg/apis/app/v1alpha1"
+	"github.com/IBM/multicloud-operators-subscription-release/pkg/utils"
+	"github.com/ghodss/yaml"
+	helmrelease "github.com/operator-framework/operator-sdk/pkg/helm/release"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+//ChartsDir env variable name which contains the directory where the charts are installed
+const ChartsDir = "CHARTS_DIR"
+
+var log = logf.Log.WithName("helmreleasemgr")
+
+//NewManager create a new manager
+func NewManager(configMap *corev1.ConfigMap, secret *corev1.Secret, s *appv1alpha1.HelmRelease) (helmrelease.Manager, error) {
+	srLogger := log.WithValues("HelmRelease.Namespace", s.Namespace, "HelmRelease.Name", s.Name)
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	o := &unstructured.Unstructured{}
+	o.SetGroupVersionKind(s.GroupVersionKind())
+	o.SetNamespace(s.GetNamespace())
+	releaseName := s.Spec.ReleaseName[0:4]
+	o.SetName(releaseName)
+	srLogger.Info("ReleaseName", "o.GetName()", o.GetName())
+	o.SetUID(s.GetUID())
+	srLogger.Info("uuid", "o.GetUID()", o.GetUID())
+
+	mgr, err := manager.New(cfg, manager.Options{
+		Namespace: s.GetNamespace(),
+		//		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	})
+	if err != nil {
+		srLogger.Error(err, "Failed to create a new manager.")
+		return nil, err
+	}
+
+	chartsDir := os.Getenv(ChartsDir)
+	if chartsDir == "" {
+		chartsDir, err = ioutil.TempDir("/tmp", "charts")
+		if err != nil {
+			srLogger.Error(err, "Can not create tempdir")
+			return nil, err
+		}
+	}
+	chartDir, err := utils.DownloadChart(configMap, secret, chartsDir, s)
+	srLogger.Info("ChartDir", "ChartDir", chartDir)
+	if err != nil {
+		srLogger.Error(err, "Failed to download the chart")
+		return nil, err
+	}
+	f := helmrelease.NewManagerFactory(mgr, chartDir)
+	if s.Spec.Values != "" {
+		var spec interface{}
+		err = yaml.Unmarshal([]byte(s.Spec.Values), &spec)
+		if err != nil {
+			srLogger.Error(err, "Failed to Unmarshal the values", "values", s.Spec.Values)
+			return nil, err
+		}
+		o.Object["spec"] = spec
+	}
+	helmManager, err := f.NewManager(o)
+	return helmManager, err
+}
