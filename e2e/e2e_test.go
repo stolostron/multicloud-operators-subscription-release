98d9bed590ec4a0d18cd6c304ef57269e82dbe41
Ian Zhang <izhang@redhat.com>
2020-11-11 21:08:04 -0500

    adding the helmrelease id
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -91,7 +91,7 @@ func TestE2ESuite(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testIDs := []string{"helm-release-001"}
+	testIDs := []string{"release-001"}
 
 	for _, tID := range testIDs {
 		if err := runner(tID); err != nil {
