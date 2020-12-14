8a2e6e8a5e45e6b0a8a19d173019dbb24084b3a3
arout <arout@redhat.com>
2020-12-14 21:43:08 +0530

    Download kubectl on macOS

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -29,11 +29,17 @@ OPERATOR_NAME=multicluster-operators-subscription-release
 if [ "$TRAVIS_BUILD" != 1 ]; then
     echo -e "Build is on Travis" 
 
-
-    echo -e "\nGet kubectl binary\n"
     # Download and install kubectl
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-
+    echo -e "\nGet kubectl binary\n"
+    PLATFORM=`uname -s | awk '{print tolower($0)}'`
+    if [ "`which kubectl`" ]; then
+        echo "kubectl PATH is `which kubectl`"
+    else
+        mkdir -p $(pwd)/bin
+        curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$PLATFORM/amd64/kubectl && mv kubectl $(pwd)/bin/ && chmod +x $(pwd)/bin/kubectl
+        export PATH=$PATH:$(pwd)/bin
+        echo "kubectl PATH is `which kubectl`"
+    fi
     COMPONENT_VERSION=$(cat COMPONENT_VERSION 2> /dev/null)
     BUILD_IMAGE=${IMAGE_NAME}:${COMPONENT_VERSION}${COMPONENT_TAG_EXTENSION}
 
