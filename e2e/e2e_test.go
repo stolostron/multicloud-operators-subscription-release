2803e5c21173982fc75a70868b6358a49f91e1e3
pezhang <pezhang@redhat.com>
2021-01-26 23:48:02 +0800

    Add release-004 to testIDs pool (#150)
    Co-authored-by: ianzhang366 <izhang@redhat.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -91,7 +91,7 @@ func TestE2ESuite(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testIDs := []string{"release-001"}
+	testIDs := []string{"release-001", "release-004"}
 
 	for _, tID := range testIDs {
 		if err := runner(tID); err != nil {
