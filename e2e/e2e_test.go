b36fbe4d5a92536049831f7dc1897f35bb15bf27
Pei Zhang <pezhang@redhat.com>
2021-01-27 16:36:02 +0800

    improve show

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -91,7 +91,7 @@ func TestE2ESuite(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testIDs := []string{"release-001","release-003", "release-004"}
+	testIDs := []string{"release-001", "release-003", "release-004"}
 
 	for _, tID := range testIDs {
 		if err := runner(tID); err != nil {
