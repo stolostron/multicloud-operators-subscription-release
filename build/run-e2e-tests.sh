004b2e6285b351ae99dc61cd853761bc34c4bf3b
Jonathan Marcantonio <jmarcant@redhat.com>
2022-06-16 10:45:56 -0400

    Trigger image rebuild to patch CVE-2022-28327
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -48,7 +48,7 @@ if [ "$TRAVIS_BUILD" != 1 ]; then
     sed -i -e "s|image: .*:latest$|image: $BUILD_IMAGE|" deploy/operator.yaml
 
     echo -e "\nDownload and install KinD\n"
-    GO111MODULE=on go get sigs.k8s.io/kind
+    GO111MODULE=on go get sigs.k8s.io/kind@v0.13.0
 else
     echo -e "\nBuild is on Local ENV, will delete the API container first\n"
     docker kill e2e || true
